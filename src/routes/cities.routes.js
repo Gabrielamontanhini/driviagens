2a2838087a7cb805335a047293440cd045f1f35e
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-09-06 17:56:19 -0300

    Feat: schema de passageiro e cidade

--- src/routes/cities.routes.js
+++ src/routes/cities.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { citiesControllers } from "../controllers/cities.controllers.js";
+import { validateSchema } from "../middlewares/validate.schema.js";
+import { citiesSchemas } from "../schemas/cities.schemas.js";
 
 const citiesRouter = Router()
 
-citiesRouter.post("/cities", citiesControllers.create)
+citiesRouter.post("/cities", validateSchema(citiesSchemas),citiesControllers.create)
 
 export default citiesRouter
