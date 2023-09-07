3b3e9c4e23ac42581bb325b88d6ab8fef10be843
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-09-06 22:52:53 -0300

    Fix: error reconhecendo o conflict

--- src/services/cities.services.js
+++ src/services/cities.services.js
@@ -3,7 +3,7 @@ import { citiesRepositories } from "../repositories/cities.repositories.js";
 
 async function create(name){
     const existingCity = await citiesRepositories.select(name)
-    if (existingCity) throw conflictError
+    if (existingCity) throw conflictError(name)
     return citiesRepositories.insert(name)
 }
 
